53f348001bd81cce368233e0f9612d9160264aca
urmom789 <nathankeele01@gmail.com>
2026-05-26 21:15:57 +0000

    May 26, 2026, 2:15 PM

--- Set.h
+++ Set.h
@@ -105,15 +105,14 @@ public:
   void remove(type item)
   {
     for (int i = 0; i < size; i++) {
-      if data[i] == item)
-        {
-          for (int j = i; j < size - 1; j++) {
-            data[j] = data[j + 1];
-          }
-
-          size--;
-          return;
+      if (data[i] == item) {
+        for (int j = i; j < size - 1; j++) {
+          data[j] = data[j + 1];
         }
+
+        size--;
+        return;
+      }
     }
   }
 }
