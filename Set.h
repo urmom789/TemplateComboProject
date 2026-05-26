0147c1c5a675ce44ee88cc71d6aabef7dae8c03a
urmom789 <nathankeele01@gmail.com>
2026-05-26 21:03:47 +0000

    May 26, 2026, 2:03 PM

--- Set.h
+++ Set.h
@@ -7,4 +7,25 @@
 
 using namespace std;
 
+template<typename type>
+class Set {
+private:
+  type* data;
+  int size;
+  int capacity;
+
+  void grow()
+  {
+    capacity *= 2;
+    type* temp = new type[capacity];
+
+    for (int i = 0; i < size; i++) {
+      temp[i] = data[i];
+    }
+
+    delete[] data;
+    data = temp;
+  }
+}
+
 #endif
