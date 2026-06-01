d4fc28a6930363e3f92749cd22f2344fbba987a1
urmom789 <nathankeele01@gmail.com>
2026-06-01 20:17:19 +0000

    Jun 1, 2026, 1:17 PM

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+void findKnightPaths(int r, int c, int movesLeft, vector<int> pathRows,
+                     vector<int> pathCols, vector<vector<int>>& boardCounts,
+                     vector<int>& rowMoves, vector<int>& colMoves)
+{
+}
+
 int main()
 {
 }
